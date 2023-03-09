2551df1fc4ba1b8fbe37e64ae3c4e869aa616a6d
Eric Echemane <eechemane29@gmail.com>
2023-03-09 09:45:01 +0800

    custom theme

--- src/contants/theme.ts
+++ src/contants/theme.ts
@@ -2,4 +2,21 @@ import { MantineThemeOverride } from '@mantine/core';
 
 export default {
 	fontFamily: 'Inter, sans-serif',
+	primaryColor: 'indigo',
+	globalStyles: (theme) => ({
+		'*': {
+			WebkitTapHighlightColor: 'transparent',
+		},
+		body: {
+			...theme.fn.fontStyles(),
+			backgroundColor: theme.colors.gray[0],
+		},
+		'.primeShadow': {
+			boxShadow: `
+				0 0.0625rem 0.1875rem ${theme.colors.indigo[0]},
+				${theme.colors.indigo[0]} 0 2.25rem 1.75rem -0.4375rem, 
+				${theme.colors.indigo[0]} 0 1.0625rem 1.0625rem -0.4375rem
+			`,
+		},
+	}),
 } as MantineThemeOverride;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Icon from '@/components/Icon';
-import { Container, Title, Text, Button } from '@mantine/core';
+import { Container, Title, Text, Button, Stack, Paper } from '@mantine/core';
 import Head from 'next/head';
 
 export default function Home() {
@@ -13,10 +13,17 @@ export default function Home() {
 				/>
 			</Head>
 
-			<Container>
-				<Title> Hello, World!</Title>
-				<Text> From Eric Echemane</Text>
-				<Button leftIcon={<Icon name='save' />}>Say Hi</Button>
+			<Container py={'lg'}>
+				<Paper
+					p={'lg'}
+					radius={'md'}
+				>
+					<Stack align={'flex-start'}>
+						<Title> Hello, World!</Title>
+						<Text> From Eric Echemane</Text>
+						<Button leftIcon={<Icon name='save' />}>Say Hi</Button>
+					</Stack>
+				</Paper>
 			</Container>
 		</>
 	);
