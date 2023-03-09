a19cb778dc73ef7ee774e8789acee7077e4fc640
Eric Echemane <eechemane29@gmail.com>
2023-03-09 11:10:30 +0800

    change google icons stylesheet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,15 +17,14 @@ export default class _Document extends Document {
 					<link
 						rel='preconnect'
 						href='https://fonts.gstatic.com'
-						crossOrigin='anonymous'
 					/>
 					<link
 						href='https://fonts.googleapis.com/css2?family=Inter&display=swap'
 						rel='stylesheet'
 					/>
 					<link
-						href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional'
 						rel='stylesheet'
+						href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0&display=swap'
 					/>
 				</Head>
 				<body>
